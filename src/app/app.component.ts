7fffc487c5858ff1327c5e737c43482358ff923e
Mattia Migliorini <mattia@visiaquantum.com>
2024-02-02 14:07:32 +0100

    Add challenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,51 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { randText } from '@ngneat/falso';
 
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [CommonModule],
+  selector: 'app-root',
+  template: `
+    <div *ngFor="let todo of todos">
+      {{ todo.title }}
+      <button (click)="update(todo)">Update</button>
+    </div>
+  `,
+  styles: [],
 })
-export class AppComponent {
-  title = 'angular-crud';
+export class AppComponent implements OnInit {
+  todos!: any[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get<any[]>('https://jsonplaceholder.typicode.com/todos')
+      .subscribe((todos) => {
+        this.todos = todos;
+      });
+  }
+
+  update(todo: any) {
+    this.http
+      .put<any>(
+        `https://jsonplaceholder.typicode.com/todos/${todo.id}`,
+        JSON.stringify({
+          todo: todo.id,
+          title: randText(),
+          body: todo.body,
+          userId: todo.userId,
+        }),
+        {
+          headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          },
+        },
+      )
+      .subscribe((todoUpdated: any) => {
+        this.todos[todoUpdated.id - 1] = todoUpdated;
+      });
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,5 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [importProvidersFrom(HttpClientModule)],
 };
